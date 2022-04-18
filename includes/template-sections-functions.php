98417d5f6ab8dc4906149e761fe454b8ff5bbbec
Dima <dimadodonov@icloud.com>
2022-04-19 00:27:46 +0400

    Fix code

--- includes/template-sections-functions.php
+++ includes/template-sections-functions.php
@@ -13,7 +13,7 @@ if ( ! function_exists( 'hook_header' ) ) {
         <div class="header-stiky__wrap">
             <div class="header-stiky__logo">
                 <?php if(is_home()) : else: ?>
-                    <a href="<?php echo get_home_url(); ?>">
+                    <a href="<?php echo home_url(); ?>">
                 <?php endif; ?>
 
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/files/icons/svg/logo.svg" alt="<?php echo get_bloginfo( 'title' ); ?>">

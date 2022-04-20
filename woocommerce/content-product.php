78b46e39043a67e7e7b8a105e8d9ddd9f5c7be9b
Dima <dimadodonov@icloud.com>
2022-04-20 23:43:05 +0400

    Work for Product Card Admin

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -32,9 +32,41 @@ $post_id = get_the_ID();
 ?>
 
 <div <?php wc_product_class('product-card product-card__loop'); ?>>
-	<?php if( current_user_can( 'edit_posts' ) ) {
-		echo '<a href="' . get_edit_post_link() . '" class="product-card__edit"><span></span></a>';
-	}; ?>
+	<?php if( current_user_can( 'edit_posts' ) ) : ?>
+		<div class="product-card__admin">
+			<?php
+				$product_name = $product->get_name();
+				$product_description = $product->get_description();
+				$product_price = $product->get_price();
+				$product_regular_price = $product->get_regular_price();
+				$product_image = $product->get_image_id();
+				$product_attributes = $product->get_attributes();
+			?>
+			<?php if( !$product_name || !$product_description || !$product_price || !$product_regular_price || !$product_image || !$product_attributes ) : ?>
+			<div class="product-card__admin--item product-card__notify">
+				<svg><use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/files/sprite.svg#icon--info"/></svg>
+				<ul>
+					<?php if(!$product_name) : ?>
+						<li>Добавьте название товара</li>
+					<?php endif;
+					if(!$product_description) : ?>
+						<li>Добавьте описание товара</li>
+					<?php endif;
+					if(!$product_price || !$product_regular_price ) : ?>
+						<li>Укажите стоимость товара</li>
+					<?php endif;
+					if(!$product_image) : ?>
+						<li>Добавить изображение товара</li>
+					<?php endif;
+					if(!$product_attributes) : ?>
+						<li>Атребуты товара не указаны</li>
+					<?php endif; ?>
+				</ul>
+			</div>
+			<?php endif; ?>
+			<a href="<?php echo get_edit_post_link(); ?>" class="product-card__admin--item product-card__edit"><span></span></a>
+		</div>
+	<?php endif; ?>
     <div class="product-card__wrap">
 
 		<figure class="product-card__thumb">
